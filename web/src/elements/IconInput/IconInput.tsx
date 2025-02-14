f0d7f2e26ee31d094eadab91135b5ff3030ca631
MBennett <90987491+mariabennett-dev@users.noreply.github.com>
2025-02-13 20:37:43 -0700

    Fixed IconInput

--- web/src/elements/IconInput/IconInput.tsx
+++ web/src/elements/IconInput/IconInput.tsx
@@ -2,7 +2,6 @@ import { useMemo } from "react";
 import styles from "./IconInput.module.css";
 
 type CustomInputProps = {
-  variant?: "primary" | "secondary";
   width?: string;
   placeholder?: string;
   value?: string | number;
@@ -15,16 +14,13 @@ type Props = React.InputHTMLAttributes<HTMLInputElement> & CustomInputProps;
 export function IconInput(props: Props) {
   const classNames = useMemo(() => {
     const classNames = [styles.input];
-    if (props.variant === "primary") classNames.push(styles.primary);
-    if (props.variant === "secondary") classNames.push(styles.secondary);
-    if (props.icon) classNames.push(styles.icon);
     return classNames;
-  }, [props.variant, props.width, props.placeholder, props.value, props.icon]);
+  }, []);
 
   return (
     <div className={styles.container} style={{ width: props.width }}>
       <view className={styles.iconInputContainer}>
-        <i>{props.icon}</i>
+        <i className={styles.icon}>{props.icon}</i>
         <input
           type="text"
           className={classNames.join(" ")} {...props}
