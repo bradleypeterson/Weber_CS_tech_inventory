cec9c2c8d7027cdfd010dc9a8235fd710300f4f1
Ben Cash <benjamincash88@gmail.com>
2025-04-15 22:35:46 -0600

    update path

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,48 +1,52 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BASE || "/",
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      workbox: {
-        globPatterns: ["**/*.{html,css,js,png,svg,jpg,webp}"]
-      },
-      strategies: "injectManifest",
-      srcDir: "src",
-      filename: "sw.ts",
-      manifest: {
-        name: "Inventory Tracker",
-        short_name: "IT",
-        description: "WSU's inventory tracker",
-        theme_color: "#fff",
-        background_color: "#fff",
-        display: "standalone",
-        icons: [
-          {
-            src: "/icons/bookcase.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ],
-        screenshots: [
-          {
-            src: "/screenshots/screenshot1.png",
-            sizes: "1606x874",
-            type: "image/png",
-            form_factor: "wide"
-          },
-          {
-            src: "/screenshots/screenshot2.png",
-            sizes: "428x712",
-            type: "image/png"
-          }
-        ]
-      }
-    })
-  ]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const base = env.VITE_BASE || "/";
+  return {
+    base,
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        workbox: {
+          globPatterns: ["**/*.{html,css,js,png,svg,jpg,webp}"]
+        },
+        strategies: "injectManifest",
+        srcDir: "src",
+        filename: "sw.ts",
+        manifest: {
+          name: "Inventory Tracker",
+          short_name: "IT",
+          description: "WSU's inventory tracker",
+          theme_color: "#fff",
+          background_color: "#fff",
+          display: "standalone",
+          icons: [
+            {
+              src: "/icons/bookcase.png",
+              sizes: "512x512",
+              type: "image/png"
+            }
+          ],
+          screenshots: [
+            {
+              src: "/screenshots/screenshot1.png",
+              sizes: "1606x874",
+              type: "image/png",
+              form_factor: "wide"
+            },
+            {
+              src: "/screenshots/screenshot2.png",
+              sizes: "428x712",
+              type: "image/png"
+            }
+          ]
+        }
+      })
+    ]
+  };
 });
