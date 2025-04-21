400e4c0d091062061a1bf8d554689442c5679ce5
M-western <55220018+M-western@users.noreply.github.com>
2025-04-20 20:03:14 -0600

    Forgot to add updated version of this file

--- web/src/api/departments.ts
+++ web/src/api/departments.ts
@@ -1,12 +1,33 @@
 import { JSONSchemaType } from "ajv";
 import type { Department } from "../../../@types/data";
 import { ajv } from "../ajv";
-import { get } from "./helpers";
+import { get, post, validateEmptyResponse } from "./helpers";
 
 export async function fetchDepartments() {
   const response = await get("/departments/list", validateResponse);
   if (response.status === "success") return response.data;
   return undefined;
+}
+
+export async function getAllDepartments() {
+  const response = await get("/departments/list", validateResponse);
+  if (response.status === "success") return response.data;
+  return undefined;
+}
+
+export async function addDepartment(department: Omit<Department, "DepartmentID">) {
+  const response = await post("/departments/add", department, validateEmptyResponse);
+  return response.status === "success";
+}
+
+export async function updateDepartment(department: Department) {
+  const response = await post(`/departments/${department.DepartmentID}/update`, department, validateEmptyResponse);
+  return response.status === "success";
+}
+
+export async function deleteDepartment(departmentId: number) {
+  const response = await post(`/departments/${departmentId}/delete`, {}, validateEmptyResponse);
+  return response.status === "success";
 }
 
 const validateResponseSchema: JSONSchemaType<Department[]> = {
